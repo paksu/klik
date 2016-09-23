74d716a59c2b00632d1b29b9e716c2eed256d341
Joona Pääkkönen <joona@giosg.com>
2016-09-24 01:00:07 +0300

    Added money display

---
+++ src/components/Money.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const getDisplay = (amount) => {
+  if(amount > 1e4 && amount < 1e6) {
+    return (amount/1000).toFixed(2) + "k";
+  } else if(amount > 1e6 && amount < 1e9) {
+    return (amount/1000000).toFixed(2) + "M";
+  }
+}
+const Money = ({amount}) => (
+  <span>{getDisplay(amount)}</span>
+)
+
+export default Money
--- src/containers/Overview.js
+++ src/containers/Overview.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getIncome } from '../selectors';
+import Money from '../components/Money';
+
 
 
 const Overview = ({money, income}) => (
   <div className="col-md-12 well well-lg">
-    <h4>Cash {money} $</h4>
+    <h4><Money amount={money} /> $</h4>
     <h4>Income <span className="text-success">{income}</span> $/second</h4>
 
   </div>
