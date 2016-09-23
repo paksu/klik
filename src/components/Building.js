70eebb8a5c87064a2725681e21fed4f1bf87a05e
Joona Pääkkönen <joona@giosg.com>
2016-09-24 01:05:56 +0300

    More money display

--- src/components/Building.js
+++ src/components/Building.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Building.css';
+import Money from './Money';
 
 const Building = ({building, onBuildingClick, canAfford}) => (
   <div className="media building" onClick={onBuildingClick} style={{'opacity': canAfford ? 1.0 : 0.5}}>
@@ -7,11 +8,11 @@ const Building = ({building, onBuildingClick, canAfford}) => (
       <i className={building.icon}></i>
     </div>
     <div className="media-body">
-      <h4 className="media-heading">{building.name} <span className="text-success">+{ building.count * building.income * building.incomeFactor}$</span></h4>
+      <h4 className="media-heading">{building.name} <span className="text-success">+<Money amount={building.count*building.income*building.incomeFactor} />$</span></h4>
       <div className="text-muted">{building.description}</div>
       <span style={{display: "inline-block"}} className="label label-success">+{building.income * building.incomeFactor} $</span>
       <span style={{display: "inline-block"}} className="label label-default">x {building.count}</span>
-      <span style={{display: "inline-block"}} className="label label-danger">Cost { building.cost}</span>
+      <span style={{display: "inline-block"}} className="label label-danger">Cost <Money amount={building.cost}/></span>
       <span style={{display: "inline-block"}} className="label label-primary">LVL { building.incomeFactor}</span>
       {building.cost10}
     </div>
--- src/components/Money.js
+++ src/components/Money.js
@@ -5,6 +5,8 @@ const getDisplay = (amount) => {
     return (amount/1000).toFixed(2) + "k";
   } else if(amount > 1e6 && amount < 1e9) {
     return (amount/1000000).toFixed(2) + "M";
+  } else {
+    return amount;
   }
 }
 const Money = ({amount}) => (
