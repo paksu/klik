573cf3bde624f0110a3775507aabffdb82bcf1d6
Joona Pääkkönen <joona@giosg.com>
2016-09-24 00:42:46 +0300

    Some refactoring

--- src/components/Upgrade.js
+++ src/components/Upgrade.js
@@ -7,7 +7,8 @@ const Upgrade = ({upgrade, onUpgradeClick, canAfford}) => (
   <div style={{opacity: canAfford ? 1.0 : 0.5, cursor: 'pointer'}} onClick={onUpgradeClick} >
     <i style={iconStyles} className={upgrade.icon}></i>
     <div style={{display: 'inline-block', marginLeft: '5px'}}>
-      {upgrade.name}
+      {upgrade.name} <span className="label label-danger">{upgrade.cost} $</span>
+      <div style={{fontWeight: 500}}>{upgrade.effect}</div>
       <p className="text-muted" style={{fontStyle: 'italic'}}>{upgrade.flavour}</p>
     </div>
   </div>
--- src/core/achievements.js
+++ src/core/achievements.js
@@ -1,4 +1,10 @@
+import { getIncome } from '../selectors';
+import _ from 'lodash';
+
 export const ACHIEVEMENTS = [{
+//
+// MONEY
+//
   conditionFn: (state) => state.money > 1000,
   achievement: {
     id: "MONEY_1",
@@ -14,12 +20,123 @@ export const ACHIEVEMENTS = [{
     icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
     description: "Reached $10000 money"
   }
-},{
-  conditionFn: (state) => state.buildings.SALES_GUY.count > 0,
+},
+
+
+//
+// INCOME
+//
+{
+  conditionFn: (state) => getIncome(state) >= 100,
   achievement: {
-    id: "HIRE_1",
-    name: "First hire",
+    id: "INCOME_1",
+    name: "Business is booming",
     icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
-    description: "Hired sales guy"
+    description: "Reached $100 income"
+  }
+},{
+  conditionFn: (state) => getIncome(state) >= 1000,
+  achievement: {
+    id: "INCOME_2",
+    name: "Business is booming 2",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Reached $1000 income"
+  }
+},{
+  conditionFn: (state) => getIncome(state) >= 10000,
+  achievement: {
+    id: "INCOME_3",
+    name: "Business is booming 3",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Reached $10000 income"
+  }
+},{
+  conditionFn: (state) => getIncome(state) >= 100000,
+  achievement: {
+    id: "INCOME_4",
+    name: "Business is booming 4",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Reached $100000 income"
+  }
+},
+
+
+//
+// ALL BUILDINGS
+//
+{
+  conditionFn: (state) => {
+    const buildingCount = _.values(state.buildings).reduce((sum, b) => b.count + sum, 0);
+    return buildingCount >= 10
+  },
+  achievement: {
+    id: "BUILDINGS_1",
+    name: "Hiring spree",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Hired 10 people"
+  }
+},{
+  conditionFn: (state) => {
+    const buildingCount = _.values(state.buildings).reduce((sum, b) => b.count + sum, 0);
+    return buildingCount >= 100
+  },
+  achievement: {
+    id: "BUILDINGS_2",
+    name: "Hiring spree 2",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Hired 100 people"
+  }
+},{
+  conditionFn: (state) => {
+    const buildingCount = _.values(state.buildings).reduce((sum, b) => b.count + sum, 0);
+    return buildingCount >= 1000
+  },
+  achievement: {
+    id: "BUILDINGS_3",
+    name: "Hiring spree 3",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Hired 1000 people"
+  }
+},{
+  conditionFn: (state) => {
+    const buildingCount = _.values(state.buildings).reduce((sum, b) => b.count + sum, 0);
+    return buildingCount >= 10000
+  },
+  achievement: {
+    id: "BUILDINGS_4",
+    name: "Hiring spree 4",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Hired 1000 people"
+  }
+},
+
+
+//
+// INDIVIDUAL BUILDINGS
+//
+
+{
+  conditionFn: (state) => state.buildings.SALES_GUY.count > 25,
+  achievement: {
+    id: "SALES_1",
+    name: "Sales team",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Hired 25 sales guys"
+  }
+},{
+  conditionFn: (state) => state.buildings.SALES_GUY.count > 100,
+  achievement: {
+    id: "SALES_2",
+    name: "Sales division",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Hired 100 sales guys guy"
+  }
+},{
+  conditionFn: (state) => state.buildings.SALES_GUY.count > 1000,
+  achievement: {
+    id: "SALES_3",
+    name: "Too much sales",
+    icon: "game-icon-green game-icon game-icon-meditation game-icon-4x",
+    description: "Hired 1000 sales guy"
   }
 }]
