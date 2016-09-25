1dbb0fa372d8232ff4d4f81f9d73ffa18ecfc18c
Joona Pääkkönen <joona@giosg.com>
2016-09-25 18:42:19 +0300

    Even better Money component

--- src/components/Money.js
+++ src/components/Money.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const getDisplay = (amount) => {
   if(amount > 1e4 && amount < 1e6) {
-    return (amount/1000).toFixed(2) + "k";
+    return (amount/1000).toFixed(3) + "k";
   } else if(amount >= 1e6 && amount < 1e9) {
-    return (amount/1e6).toFixed(2) + "M";
+    return (amount/1e6).toFixed(3) + "M";
   } else if(amount >= 1e9 && amount < 1e12) {
-    return (amount/1e9).toFixed(2) + "B";
+    return (amount/1e9).toFixed(3) + "B";
   } else if(amount >= 1e12 && amount < 1e15) {
-    return (amount/1e12).toFixed(2) + "T";
+    return (amount/1e12).toFixed(3) + "T";
   } else {
     return amount;
   }
