a699407c0b2d1b3916f86495f1d98a3551f7ae8a
Joona Pääkkönen <joona@giosg.com>
2016-09-25 16:10:17 +0300

    Improved Money

--- src/components/Money.js
+++ src/components/Money.js
@@ -3,8 +3,12 @@ import React from 'react'
 const getDisplay = (amount) => {
   if(amount > 1e4 && amount < 1e6) {
     return (amount/1000).toFixed(2) + "k";
-  } else if(amount > 1e6 && amount < 1e9) {
-    return (amount/1000000).toFixed(2) + "M";
+  } else if(amount >= 1e6 && amount < 1e9) {
+    return (amount/1e6).toFixed(2) + "M";
+  } else if(amount >= 1e9 && amount < 1e12) {
+    return (amount/1e9).toFixed(2) + "B";
+  } else if(amount >= 1e12 && amount < 1e15) {
+    return (amount/1e12).toFixed(2) + "T";
   } else {
     return amount;
   }
