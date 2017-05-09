698ce6dda69e4891316d634224ee95c920a6578f
Joona Pääkkönen <joona@giosg.com>
2017-05-09 12:21:03 +0300

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# Klik
-An endless game project to experiment with React/Redux combination
-
-```
-npm install && npm start
-```
--- src/core/helpers.js
+++
@@ -1,6 +0,0 @@
-import _ from 'lodash';
-
-export const getIncome = (state) =>  {
-  // Calculates income from all buildings for a single tick
-  return _.values(state.buildings).reduce((income, b) => b.count * b.income * b.incomeFactor + income, 0);
-}
